e434e97acd93ecdfe7af0617371cc174fdfaf38e
Tyler Lartonoix <larttyler@live.com>
2016-01-27 15:34:47 -0500

    Update publishing sample to mimic README code

--- samples/publishing.php
+++ samples/publishing.php
@@ -3,36 +3,60 @@
 
 	use DaybreakStudios\Analyst\Experiment;
 	use DaybreakStudios\Analyst\ResultInterface;
+	use PDO;
 
 	require __DIR__ . '/bootstrap.php';
 
+	define('DB_HOST', '127.0.0.1');
+	define('DB_NAME', 'analyst_demo');
+	define('DB_USER', 'root');
+	define('DB_PASS', '');
+
 	class MyExperiment extends Experiment {
+		private $db;
+		private $stmt;
+
+		public function __construct($name, PDO $db) {
+			parent::__construct($name);
+
+			$this->db = $db;
+			$this->stmt = $db->prepare('insert into experiment_results (experiment, timestamp, behavior, is_control,
+				execution_time, result) values (:experiment, UTC_TIMESTAMP, :behavior, :control, :duration, :result)');
+
+			$this->stmt->bindValue(':experiment', $name);
+		}
+
 		public function publish(ResultInterface $result) {
-			printf(":: Name: %s\n", parent::getName());
-			printf("Observations made: %d\n", sizeof($result->getObservations()));
-			printf("Mismatches? %d\n", sizeof($result->getMismatches()));
+			$behaviors = $result->getCandidates();
 
-			printf(":: Observation Results\n");
+			array_unshift($behaviors, $result->getControl());
 
-			foreach ($result->getObservations() as $ob)
-				printf("\t%s: %s\n", $ob->getName(), $ob->hasException() ? 'Exception thrown: ' .
-					get_class($ob->getException()) : getVarDumpValue($ob->getResult()));
+			$this->db->beginTransaction();
+
+			foreach ($behaviors as $behavior) {
+				$this->stmt->bindValue(':behavior', $behavior->getName());
+				$this->stmt->bindValue(':control', $behavior === $result->getControl(), PDO::PARAM_BOOL);
+				$this->stmt->bindValue(':duration', $behavior->getDuration());
+				$this->stmt->bindValue(':result', serialize($behavior->getResult()));
+
+				$this->stmt->execute();
+			}
+
+			$this->db->commit();
+		}
+
+		public static function create($name = 'experiment', PDO $db) {
+			return new MyExperiment($name, $db);
 		}
 	}
 
-	$a = MyExperiment::create('experiment-a')
-		->control(function() { return 1; })
-		->candidate(function() { return 1; })
-		->run();
-
-	printf("Result: %s\n\n", getVarDumpValue($a));
-
-	$b = MyExperiment::create('experiment-b')
+	$a = MyExperiment
+		::create('my-experiment', new PDO(sprintf('mysql:dbname=%s;host=%s', DB_NAME, DB_HOST), DB_USER, DB_PASS))
 		->control(function() { return 1; })
 		->candidate(function() { return '1'; })
 		->run();
 
-	printf("Result %s\n", getVarDumpValue($b));
+	printf("Result: %s\n\n", getVarDumpValue($a));
 
 	function getVarDumpValue($val) {
 		ob_start();
